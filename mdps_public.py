032e61f9fb9a7ace552e4d0a7e89f4848072f80b
Abhirup257 <abhirup257.hit16@gmail.com>
2025-02-13 11:01:30 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -25,9 +25,8 @@ with st.sidebar:
     selected = option_menu('Multiple Disease Prediction System',
                           
                           ['Diabetes Prediction',
-                           'Heart Disease Prediction',
                            'Parkinsons Prediction'],
-                          icons=['activity','heart','person'],
+                          icons=['activity','person'],
                           default_index=0)
     
     
